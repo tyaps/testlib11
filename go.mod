c0ad2078038b4c9ef27af4e9782692706654d107
Sergey Tyapkin <s.tyapkin@omprussia.ru>
2022-07-18 14:28:58 +0300

    11

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tyaps/testlib11
+
+go 1.18
+
+require github.com/tyaps/testlib2 v1.0.1 // indirect
---
+++ lib1.go
@@ -0,0 +1,7 @@
+package testlib11
+
+import "github.com/tyaps/testlib2"
+
+func PrintName(s string) {
+	testlib2.PrintName("aaa")
+}
