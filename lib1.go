bf6dcded260ccbbfd1efc500e8c703427337d942
Sergey Tyapkin <s.tyapkin@omprussia.ru>
2022-07-18 14:40:34 +0300

    22

--- lib1.go
+++ lib1.go
@@ -1,7 +1,15 @@
 package testlib11
 
-import "github.com/tyaps/testlib2"
+import (
+	"fmt"
+
+	"github.com/tyaps/testlib2"
+)
 
 func PrintName(s string) {
 	testlib2.PrintName("aaa")
+}
+
+func PrintAny(s string) {
+	fmt.Printf("some print by myself: testlib11")
 }
